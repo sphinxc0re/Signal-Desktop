7b88322ca83ffef29b141564bda683015cb83d90
Julian Laubstein <contact@julianlaubstein.de>
2019-08-06 11:17:28 +0200

    Added qrcode library

--- app/encryption_channel.js
+++ app/encryption_channel.js
@@ -1,11 +1,15 @@
 const electron = require('electron');
 const base64 = require('base64-arraybuffer');
+const openpgp = require('openpgp');
+const QRCode = require('qrcode');
 
 const { ipcMain } = electron;
 
 module.exports = {
   initialize,
 };
+
+let keyStore = {};
 
 let initialized = false;
 
@@ -18,6 +22,8 @@ const keyWithPrefix = key => [PREFIX, key].join(DELIMITER);
 // ipc keys
 const ENCRYPTION_REQUEST = keyWithPrefix('encrypt-attachment');
 const DECRYPTION_REQUEST = keyWithPrefix('decrypt-attachment');
+
+const KEY_GENERATION_REQUEST = keyWithPrefix('generate-key');
 
 function initialize() {
   if (initialized) {
@@ -39,5 +45,19 @@ function initialize() {
   ipcMain.on(DECRYPTION_REQUEST, (event, { data }) => {
     // TODO: decryprtion
     event.returnValue = { data };
+  });
+
+  ipcMain.on(KEY_GENERATION_REQUEST, async () => {
+    const options = {
+      userIds: [{ name: 'Jon Smith', email: 'jon@example.com' }],
+      curve: 'ed25519',
+      passphrase: 'super long and hard to guess secret',
+    };
+
+    const key = await openpgp.generateKey(options);
+
+    keyStore = { privKey: key };
+
+    event.returnValue = await QRCode.toDataURL(key.publicKeyArmored);
   });
 }
